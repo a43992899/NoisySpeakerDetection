df8f6f98fdee8787b9df0728ad3649ad6d713734
a43992899 <a43992899@163.com>
2022-03-13 03:59:16 -0400

    add xvector noisy label gen, fix data_preprocess.py

---
+++ recipes/VoxCeleb/NoisySpeakerDet/xvector/Corrupt/Softmax/data_load_test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  6 20:55:52 2018
+
+@author: harry
+"""
+import glob
+import numpy as np
+import os
+import random
+from random import shuffle
+import torch
+from torch.utils.data import Dataset
+
+from hparam import hparam as hp
+from utils import mfccs_and_spec
+import time, json
+
+class SpeakerDatasetPreprocessedTest(Dataset):
+    
+    def __init__(self, shuffle=True, utter_start=0):
+        
+        # data path
+        if hp.training:
+            print(hp.data.train_path)
+            self.path = hp.data.train_path
+            self.utter_num = hp.train.M
+        else:
+            self.path = hp.data.test_path
+            self.utter_num = hp.test.M
+        self.file_list = os.listdir(self.path)
+        
+        random.seed(time.time())
+        random.shuffle(self.file_list)
+        self.length = int(len(self.file_list)*1)
+        self.file_list = self.file_list[:self.length]
+        self.shuffle=shuffle
+        self.utter_start = utter_start
+        
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        np_file_list = self.file_list
+
+        if self.shuffle:
+            random.seed(time.time())
+            selected_file = random.sample(np_file_list, 1)[0]  # select random speaker
+        else:
+            selected_file = np_file_list[idx]               
+            
+        utters = np.load(os.path.join(self.path, selected_file))        # load utterance spectrogram of selected speaker
+        if self.shuffle:
+            utter_index = np.random.randint(0, utters.shape[0], self.utter_num)   # select M utterances per speaker
+            utterance = utters[utter_index]  
+        else:
+            utterance = utters[self.utter_start: self.utter_start+self.utter_num] # utterances of a speaker [batch(M), n_mels, frames]
+
+        utterance = utterance[:,:,:160]               # TODO implement variable length batch size
+
+        utterance = torch.tensor(np.transpose(utterance, axes=(0,2,1)))     # transpose [batch, frames, n_mels]
+        
+        return utterance
---
+++ recipes/VoxCeleb/NoisySpeakerDet/xvector/data_preprocess.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#Modified from https://github.com/JanhHyun/Speaker_Verification
+import os, sys, glob
+# set current working directory
+os.chdir('/home/yrb/code/speechbrain/recipes/VoxCeleb/NoisySpeakerDet/xvector/Split/')
+sys.path.append('/home/yrb/code/speechbrain/recipes/VoxCeleb/NoisySpeakerDet/xvector/Split/')
+
+import librosa
+import numpy as np
+from hparam import hparam as hp
+from tqdm import tqdm
+import warnings
+warnings.filterwarnings("ignore")
+
+# downloaded dataset path                                
+
+def save_spectrogram_tisv(speaker_path, data_train_path):
+    
+    utter_min_len = (hp.data.tisv_frame * hp.data.hop + hp.data.window) * hp.data.sr    # lower bound of utterance length
+    total_speaker_num = len(os.listdir(speaker_path))
+    train_speaker_num= (total_speaker_num//10)*9            # split total data 90% train and 10% test
+    print("total speaker number : %d"%total_speaker_num)
+    print("train : %d, test : %d"%(train_speaker_num, total_speaker_num-train_speaker_num))
+    for i, folder in enumerate(tqdm(os.listdir(speaker_path))):
+        # print(folder)
+        # print("%dth speaker processing..."%i)
+        utterances_spec = []
+        for video in os.listdir(os.path.join(speaker_path,folder)):
+            for utter_name in os.listdir(os.path.join(speaker_path,folder, video)):          
+                
+                utter_path = os.path.join(speaker_path,folder, video, utter_name)         # path of each utterance
+                    
+                utter, sr = librosa.core.load(utter_path, hp.data.sr)        # load utterance audio
+                    
+                intervals = librosa.effects.split(utter, top_db=30)         # voice activity detection
+                for interval in intervals:
+                    if (interval[1]-interval[0]) > utter_min_len:           # If partial utterance is sufficient long
+                        utter_part = utter[interval[0]:interval[1]]         # save first and last 180 frames of spectrogram.
+                        S = librosa.core.stft(y=utter_part, n_fft=hp.data.nfft,
+                                                win_length=int(hp.data.window * sr), hop_length=int(hp.data.hop * sr))
+                        S = np.abs(S) ** 2
+                        mel_basis = librosa.filters.mel(sr=hp.data.sr, n_fft=hp.data.nfft, n_mels=hp.data.nmels)
+                        S = np.log10(np.dot(mel_basis, S) + 1e-6)           # log mel spectrogram of utterances
+                        utterances_spec.append(S[:, :hp.data.tisv_frame])    # first 180 frames of partial utterance
+                        utterances_spec.append(S[:, -hp.data.tisv_frame:])   # last 180 frames of partial utterance
+
+        utterances_spec = np.array(utterances_spec)
+        np.save(os.path.join(data_train_path, "{}.npy".format(folder)), utterances_spec)
+
+if __name__ == "__main__":
+    save_spectrogram_tisv("/home/yrb/code/speechbrain/data/voxceleb/vox2/aac","/home/yrb/code/speechbrain/data/voxceleb/vox2/spmel")
