fix aam softmax para

Permute/GE2E/data_load.py
@@ -36,6 +36,7 @@ class SpeakerDatasetPreprocessed(Dataset):
         else:
             self.path = hp.data.test_path
             self.utter_num = hp.test.M
+        self.noise_level = hp.train.noise_level
         self.get_spkr_id()
         self.length = int(len(self.spkr2id)*1)
         self.get_spkr2utter()
@@ -57,9 +58,14 @@ class SpeakerDatasetPreprocessed(Dataset):
         """
         self.file_list = os.listdir(self.path)
         self.file_list.sort()
-        # mislabel_mapper maps a file to a wrong speaker
-        with open("/home/yrb/code/speechbrain/Permute/Softmax/voxceleb2_20%_mislabel.json", "r") as f:
-            mislabel_mapper = json.load(f)
+        assert self.noise_level in [0, 20, 40, 50 ,60, 75]
+        if self.noise_level > 0:
+            # mislabel_mapper maps a file to a wrong speaker
+            with open(f"/home/yrb/code/speechbrain/Permute/Softmax/voxceleb2_{self.noise_level}%_mislabel.json", "r") as f:
+                mislabel_mapper = json.load(f)
+        else:
+            mislabel_mapper = {}
+        
         self.spkr2utter = {}
         self.spkr2utter_mislabel = {}
         print("Loading spkr2utter and spkr2utter_mislabel...")
@@ -69,6 +75,7 @@ class SpeakerDatasetPreprocessed(Dataset):
                 self.spkr2utter[speaker_id] = []
             self.spkr2utter[speaker_id].append(file)
 
+            # if noise level is 0, then spkr2utter = spkr2utter_mislabel
             speaker_id = mislabel_mapper.get(file[:-4], speaker_id)
             if speaker_id not in self.spkr2utter_mislabel:
                 self.spkr2utter_mislabel[speaker_id] = []
@@ -85,7 +92,10 @@ class SpeakerDatasetPreprocessed(Dataset):
             utters = self.spkr2utter[selected_spkr]
         
         # select self.utter_num samples from utters
-        selected_utters = random.sample(utters, self.utter_num)
+        if self.utter_num > 0:
+            selected_utters = random.sample(utters, self.utter_num)
+        else:
+            selected_utters = utters
         utterance_final, speaker_label, is_noisy, utterance_id = [], [], [], []
         for utter in selected_utters:
             data = np.load(os.path.join(self.path, utter))
@@ -101,58 +111,3 @@ class SpeakerDatasetPreprocessed(Dataset):
         utterance = torch.tensor(np.transpose(utterance, axes=(0,2,1))) # transpose [batch, frames, n_mels]
         return utterance, np.array(speaker_label), np.array(is_noisy), utterance_id
 
-
-class SpeakerDatasetPreprocessed_old(Dataset):
-    
-    def __init__(self, shuffle=True, utter_start=0):
-        
-        # data path
-        if hp.training:
-            self.path = hp.data.train_path
-            self.utter_num = hp.train.M
-        else:
-            self.path = hp.data.test_path
-            self.utter_num = hp.test.M
-        self.file_list = os.listdir(self.path)
-        
-        random.shuffle(self.file_list)
-        self.length = int(len(self.file_list)*1)
-        self.file_list = self.file_list[:self.length]
-        self.shuffle=shuffle
-        self.utter_start = utter_start
-        with open("/home/yrb/code/speechbrain/Permute/Softmax/voxceleb2_20%_mislabel.json", "r") as f:
-            self.mislabel_dict = json.load(f)
-        
-    def __len__(self):
-        return self.length
-
-    def __getitem__(self, idx):
-        np_file_list = os.listdir(self.path)
-
-        selected_file = random.sample(np_file_list, 1)[0]  # select random speaker
-            
-        utters = np.load(os.path.join(self.path, selected_file))        # load utterance spectrogram of selected speaker
-       
-        utter_index = np.random.randint(0, utters.shape[0], self.utter_num)   # select M utterances per speaker
-            
-        utterance = utters[utter_index]  
-
-        utterance_final = []
-        for index in utter_index:
-            packet = relabel(selected_file[:-4], index, self.mislabel_dict)
-            if(packet is None): #not corrupted
-                utterance_final.extend(utters[np.array([index])])
-            else: #corrupted
-                new_selected_file = packet[0] + ".npy"
-                new_utters = np.load(os.path.join(self.path, new_selected_file))
-                new_utter_index = np.array([packet[1]])
-                new_utterance = new_utters[new_utter_index]
-                utterance_final.extend(new_utterance)
-
-        utterance = np.array(utterance_final)
-
-        utterance = utterance[:,:,:160]               # TODO implement variable length batch size
-
-        utterance = torch.tensor(np.transpose(utterance, axes=(0,2,1)))     # transpose [batch, frames, n_mels]
-        
-        return utterance

Permute/GE2E/speech_embedder_net.py
@@ -204,8 +204,8 @@ class AngularPenaltySMLoss(nn.Module):
         loss_type = loss_type.lower()
         assert loss_type in  ['arcface', 'sphereface', 'cosface']
         if loss_type == 'arcface':
-            self.s = 64.0 if not s else s
-            self.m = 0.5 if not m else m
+            self.s = 15.0 if not s else s
+            self.m = 0.3 if not m else m
         if loss_type == 'sphereface':
             self.s = 64.0 if not s else s
             self.m = 1.35 if not m else m