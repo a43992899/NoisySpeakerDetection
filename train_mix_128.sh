754611a1a53b2b1e3778a7b2efab567e25e1502f
Hanzhi Yin <yinhanzhi@gmail.com>
2022-07-06 15:35:57 -0400

    add exec permission to all shell script

--- train_mix_128.sh
+++ train_mix_128.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # /home/yrb/miniconda3/envs/speechbrain_ENV/bin/python /home/yrb/code/speechbrain/core/train_speech_embedder.py --cfg /home/yrb/code/speechbrain/data/models/Mix/Softmax/20%/bs128/log/config.yaml
 # /home/yrb/miniconda3/envs/speechbrain_ENV/bin/python /home/yrb/code/speechbrain/core/train_speech_embedder.py --cfg /home/yrb/code/speechbrain/data/models/Mix/Softmax/50%/bs128/log/config.yaml
 # /home/yrb/miniconda3/envs/speechbrain_ENV/bin/python /home/yrb/code/speechbrain/core/train_speech_embedder.py --cfg /home/yrb/code/speechbrain/data/models/Mix/Softmax/75%/bs128/log/config.yaml
--- train_mix_256.sh
+++ train_mix_256.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 /home/yrb/miniconda3/envs/speechbrain_ENV/bin/python /home/yrb/code/speechbrain/core/train_speech_embedder.py --cfg /home/yrb/code/speechbrain/data/models/Mix/Softmax/20%/bs256/log/config.yaml
 /home/yrb/miniconda3/envs/speechbrain_ENV/bin/python /home/yrb/code/speechbrain/core/train_speech_embedder.py --cfg /home/yrb/code/speechbrain/data/models/Mix/Softmax/50%/bs256/log/config.yaml
 /home/yrb/miniconda3/envs/speechbrain_ENV/bin/python /home/yrb/code/speechbrain/core/train_speech_embedder.py --cfg /home/yrb/code/speechbrain/data/models/Mix/Softmax/75%/bs256/log/config.yaml
