40a2881e59d06a5dc5f3917714cc709c1df6fc8f
Hanzhi Yin <yinhanzhi@gmail.com>
2022-07-07 10:32:01 -0400

    refactor all the rest of the code

--- core/data_load.py
+++ core/data_load.py
@@ -5,29 +5,29 @@ Created on Mon Aug  6 20:55:52 2018
 
 @author: harry
 """
-import glob
-import numpy as np
+import json
 import os
 import random
-from random import shuffle
+from tqdm import tqdm
+
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-from utils import mfccs_and_spec
-import time, json
-from tqdm import tqdm
 
 random.seed(1)
 np.random.seed(1)
 
+
+# TODO: unused function?
 def relabel(selected_file, utter_index, mislabel_dict):
     key = selected_file + "_{}".format(utter_index)
-    if(key in mislabel_dict):
+    if key in mislabel_dict:
         return mislabel_dict[key]
-    return None
+
 
 class SpeakerDatasetPreprocessed(Dataset):
-    
+
     def __init__(self, hp):
         self.vox1_path = hp.data.vox1_path
         self.stage = hp.stage
@@ -36,12 +36,12 @@ class SpeakerDatasetPreprocessed(Dataset):
             self.utter_num = hp.train.M
             self.noise_type = hp.train.noise_type
             self.noise_level = hp.train.noise_level
-        elif self.stage == "test": # test EER
+        elif self.stage == "test":  # test EER
             self.path = hp.data.test_path
             self.utter_num = hp.test.M
             self.noise_type = None
             self.noise_level = 0
-        elif self.stage == "nld": # noise label detection
+        elif self.stage == "nld":  # noise label detection
             self.path = hp.data.nld_path
             self.utter_num = hp.nld.M
             self.noise_type = hp.nld.noise_type
@@ -51,17 +51,19 @@ class SpeakerDatasetPreprocessed(Dataset):
         assert self.noise_type in ["Permute", "Open", "Mix", None], "noise_type should be Permute/Open/Mix/None"
         assert self.noise_level in [0, 20, 50, 75], "noise_level should be 0/20/50/75"
         self.get_spkr_id()
-        self.length = int(len(self.spkr2id)*1)
+        self.length = int(len(self.spkr2id) * 1)
         self.get_spkr2utter()
 
     def isFilename(self, inp):
+        # TODO: consider using os.path.isfile?
+        os.path.isfile(inp)
         return '.' in inp
-    
+
     def get_spkr_id(self):
         spkr2id_json = os.path.join(self.path, "../spkr2id.json")
         self.spkr2id = json.load(open(spkr2id_json, "r"))
         self.id2spkr = {v: k for k, v in self.spkr2id.items()}
-    
+
     def get_spkr2utter(self):
         """
         This function is used to get the correct and mislabeled spkr2utter dict
@@ -76,7 +78,7 @@ class SpeakerDatasetPreprocessed(Dataset):
                 mislabel_mapper = json.load(f)
         else:
             mislabel_mapper = {}
-        
+
         self.spkr2utter = {}
         self.spkr2utter_mislabel = {}
         print("Loading spkr2utter and spkr2utter_mislabel...")
@@ -102,7 +104,7 @@ class SpeakerDatasetPreprocessed(Dataset):
                 if speaker_id not in self.spkr2utter_mislabel:
                     self.spkr2utter_mislabel[speaker_id] = []
                 self.spkr2utter_mislabel[speaker_id].append(os.path.join(self.path, file))
-        
+
     def __len__(self):
         return self.length
 
@@ -112,7 +114,7 @@ class SpeakerDatasetPreprocessed(Dataset):
             utters = self.spkr2utter_mislabel[selected_spkr]
         else:
             utters = self.spkr2utter[selected_spkr]
-        
+
         # select self.utter_num samples from utters
         if self.utter_num > 0:
             selected_utters = random.sample(utters, self.utter_num)
@@ -130,7 +132,6 @@ class SpeakerDatasetPreprocessed(Dataset):
                 is_noisy.extend([1])
             utterance_id.extend([utter_name])
         utterance = np.array(utterance_final)
-        utterance = utterance[:,:,:160]
-        utterance = torch.tensor(np.transpose(utterance, axes=(0,2,1))) # transpose [batch, frames, n_mels]
+        utterance = utterance[:, :, :160]
+        utterance = torch.tensor(np.transpose(utterance, axes=(0, 2, 1)))  # transpose [batch, frames, n_mels]
         return utterance, np.array(speaker_label), np.array(is_noisy), utterance_id
-
