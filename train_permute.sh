c5d74851a7d97328ff612a2361e1d8555b08e7a9
a43992899 <a43992899@163.com>
2022-10-16 20:44:44 -0400

    fix ge2e m=1 bug

--- train_permute.sh
+++ train_permute.sh
@@ -6,8 +6,6 @@ python . train CE 20 Permute --N 128 --M 1 --random-seed ${SEED}
 python . train CE 20 Permute --N 256 --M 1 --random-seed ${SEED}
 python . train AAM 20 Permute --N 128 --M 1 --s 15 --m 0.1 --random-seed ${SEED}
 python . train AAM 20 Permute --N 256 --M 1 --s 15 --m 0.1 --random-seed ${SEED}
-python . train GE2E 20 Permute --N 128 --M 1 --random-seed ${SEED}
-python . train GE2E 20 Permute --N 256 --M 1 --random-seed ${SEED}
 python . train GE2E 20 Permute --N 64 --M 4 --random-seed ${SEED}
 python . train GE2E 20 Permute --N 32 --M 4 --random-seed ${SEED}
 python . train GE2E 20 Permute --N 32 --M 8 --random-seed ${SEED}
@@ -22,8 +20,6 @@ python . train CE 50 Permute --N 128 --M 1 --random-seed ${SEED}
 python . train CE 50 Permute --N 256 --M 1 --random-seed ${SEED}
 python . train AAM 50 Permute --N 128 --M 1 --s 15 --m 0.1 --random-seed ${SEED}
 python . train AAM 50 Permute --N 256 --M 1 --s 15 --m 0.1 --random-seed ${SEED}
-python . train GE2E 50 Permute --N 128 --M 1 --random-seed ${SEED}
-python . train GE2E 50 Permute --N 256 --M 1 --random-seed ${SEED}
 python . train GE2E 50 Permute --N 64 --M 4 --random-seed ${SEED}
 python . train GE2E 50 Permute --N 32 --M 4 --random-seed ${SEED}
 python . train GE2E 50 Permute --N 32 --M 8 --random-seed ${SEED}
@@ -38,8 +34,6 @@ python . train CE 75 Permute --N 128 --M 1 --random-seed ${SEED}
 python . train CE 75 Permute --N 256 --M 1 --random-seed ${SEED}
 python . train AAM 75 Permute --N 128 --M 1 --s 15 --m 0.1 --random-seed ${SEED}
 python . train AAM 75 Permute --N 256 --M 1 --s 15 --m 0.1 --random-seed ${SEED}
-python . train GE2E 75 Permute --N 128 --M 1 --random-seed ${SEED}
-python . train GE2E 75 Permute --N 256 --M 1 --random-seed ${SEED}
 python . train GE2E 75 Permute --N 64 --M 4 --random-seed ${SEED}
 python . train GE2E 75 Permute --N 32 --M 4 --random-seed ${SEED}
 python . train GE2E 75 Permute --N 32 --M 8 --random-seed ${SEED}
