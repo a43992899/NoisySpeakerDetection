17fd6e20d8fff71d8a9ecefd3ccbf673823b68ac
Hanzhi Yin <yinhanzhi@gmail.com>
2022-10-18 22:51:12 -0400

    produce speaker dataset per utterance

--- nld/model/loss.py
+++ nld/model/loss.py
@@ -9,6 +9,11 @@ from ..utils import accuracy
 
 
 class GE2ELoss(nn.Module):
+    
+    w: nn.Parameter
+    b: nn.Parameter
+    criterion: nn.CrossEntropyLoss
+
     def __init__(self, init_w=10.0, init_b=-5.0):
         super(GE2ELoss, self).__init__()
 
@@ -18,10 +23,7 @@ class GE2ELoss(nn.Module):
         self.b = nn.Parameter(torch.tensor(init_b))
         self.criterion = torch.nn.CrossEntropyLoss()
 
-        print('Initialised GE2E')
-
     def forward(self, x: Tensor, label=None):
-
         assert x.size()[1] >= 2
 
         gsize = x.size()[1]
@@ -33,37 +35,6 @@ class GE2ELoss(nn.Module):
         for ii in range(0, gsize):
             idx = [*range(0, gsize)]
             idx.remove(ii)
-            exc_centroids = torch.mean(x[:, idx, :], 1)
-            cos_sim_diag = F.cosine_similarity(x[:, ii, :], exc_centroids)
-            cos_sim = F.cosine_similarity(x[:, ii, :].unsqueeze(-1), centroids.unsqueeze(-1).transpose(0, 2))
-            cos_sim[range(0, stepsize), range(0, stepsize)] = cos_sim_diag
-            cos_sim_matrix.append(torch.clamp(cos_sim, 1e-6))
-
-        cos_sim_matrix = torch.stack(cos_sim_matrix, dim=1)
-
-        torch.clamp(self.w, 1e-6)
-        cos_sim_matrix = cos_sim_matrix * self.w + self.b
-
-        label = torch.from_numpy(np.asarray(range(0, stepsize))).cuda()
-        nloss = self.criterion(cos_sim_matrix.view(-1, stepsize),
-                               torch.repeat_interleave(label, repeats=gsize, dim=0).cuda())
-        prec1 = accuracy(cos_sim_matrix.view(-1, stepsize).detach(),
-                         torch.repeat_interleave(label, repeats=gsize, dim=0).detach(), topk=(1,))[0]
-        return nloss, prec1
-
-    def get_confidence(self, x: Tensor):
-        assert x.size()[1] >= 2
-
-        stepsize = x.size()[0]
-        gsize = x.size()[1]  # in our case, gsize is number of  utterance
-        centroids = torch.mean(x, 1)  # self-included centroids
-
-        cos_sim_matrix = []
-
-        for ii in range(0, gsize):
-            idx = [*range(0, gsize)]
-            idx.remove(ii)
-            # Calculate the self-excluded centroids.
             exc_centroids = torch.mean(x[:, idx, :], 1)
             cos_sim_diag = F.cosine_similarity(x[:, ii, :], exc_centroids)
             cos_sim = F.cosine_similarity(x[:, ii, :].unsqueeze(-1), centroids.unsqueeze(-1).transpose(0, 2))
--- nld/process_data/dataset.py
+++ nld/process_data/dataset.py
@@ -11,6 +11,78 @@ from torch.utils.data import Dataset
 VOX2_CLASS_NUM = 5994
 
 
+class SpeakerUtteranceDataset(Dataset):
+    noise_mel_spectrogram_dir: Optional[Path]
+    main_mel_spectrogram_dir: Path
+
+    speaker_utterances: List[str]
+    speaker_label_to_id: Dict[str, int]
+    speaker_labels: List[str]
+    mislabeled_mapping: Optional[Dict[str, str]]
+
+    def __init__(
+        self, noise_mel_spectrogram_dir: Optional[Path], main_mel_spectrogram_dir: Path,
+        mislabeled_json_file: Optional[Path],
+    ):
+        super().__init__()
+
+        self.noise_mel_spectrogram_dir = noise_mel_spectrogram_dir
+        self.main_mel_spectrogram_dir = main_mel_spectrogram_dir
+
+        if mislabeled_json_file is not None:
+            with open(mislabeled_json_file, 'r') as f:
+                self.mislabeled_mapping = json.load(f)
+        else:
+            self.mislabeled_mapping = None
+
+        with open(main_mel_spectrogram_dir / 'speaker-label-to-id.json', 'r') as f:
+            self.speaker_label_to_id = json.load(f)
+        self.speaker_labels = sorted(self.speaker_label_to_id.keys())
+
+        self.speaker_utterances = [
+            p.name for p in main_mel_spectrogram_dir.iterdir() if p.suffix == '.npy'
+        ]
+
+    def __len__(self):
+        return len(self.speaker_utterances)
+
+    def __getitem__(self, index: int):
+        selected_speaker_utterance = self.speaker_utterances[index]
+        original_utterance_path = self.main_mel_spectrogram_dir / selected_speaker_utterance
+        original_label = selected_speaker_utterance.split('-')[0]
+
+        if self.mislabeled_mapping is not None and selected_speaker_utterance in self.mislabeled_mapping:
+            assert self.noise_mel_spectrogram_dir is not None
+            is_noisy = True
+            mislabeled = self.mislabeled_mapping[selected_speaker_utterance]
+            if mislabeled.endswith('.npy'):
+                tainted_utterane_path = self.noise_mel_spectrogram_dir / mislabeled
+                spectrogram = np.load(tainted_utterane_path).transpose()
+                label = original_label
+            else:
+                tainted_utterane_path = original_utterance_path
+                spectrogram = np.load(original_utterance_path)
+                label = mislabeled
+        else:
+            is_noisy = False
+            tainted_utterane_path = original_utterance_path
+            spectrogram = np.load(original_utterance_path).transpose()
+            label = original_label
+
+        selected_id = self.speaker_label_to_id[label]
+        return torch.from_numpy(spectrogram), is_noisy, selected_id, tainted_utterane_path, original_utterance_path
+
+    @staticmethod
+    def collate_fn(batch: Sequence[Tensor, bool, int, Path, Path]):
+        return (
+            torch.stack([b[0] for b in batch]),
+            torch.tensor([b[1] for b in batch]),
+            torch.tensor([b[2] for b in batch]),
+            [b[3] for b in batch],
+            [b[4] for b in batch],
+        )
+
+
 class SpeakerDataset(Dataset):
     sample_num: int
     noise_mel_spectrogram_dir: Optional[Path]
@@ -18,7 +90,7 @@ class SpeakerDataset(Dataset):
 
     speaker_label_to_utterances: Dict[str, List[str]]
     speaker_label_to_id: Dict[str, int]
-    speaker_labels = List[str]
+    speaker_labels: List[str]
 
     mislabeled_mapping: Optional[Dict[str, str]]
 
@@ -73,6 +145,7 @@ class SpeakerDataset(Dataset):
         for i in range(self.sample_num):
             vox2_selected_file = selected_speaker_mel_spectrogram_files[i]
             if self.mislabeled_mapping is not None and vox2_selected_file in self.mislabeled_mapping:
+                assert self.noise_mel_spectrogram_dir is not None
                 selected_file_is_noisy[i] = True
                 mislabeled = self.mislabeled_mapping[vox2_selected_file]
                 if mislabeled.endswith('.npy'):
@@ -99,7 +172,7 @@ class SpeakerDataset(Dataset):
         )
 
     @staticmethod
-    def collate_fn(batch: Sequence[Tuple[Tensor, Tensor, Tensor, List[str], List[str]]]):
+    def collate_fn(batch: Sequence[Tuple[Tensor, Tensor, Tensor, List[Path], List[Path]]]):
         return (
             torch.stack([b[0] for b in batch]),
             torch.stack([b[1] for b in batch]),
