241037139739b6c1188720e849703e6f71960c89
Hanzhi Yin <yinhanzhi@gmail.com>
2022-10-28 12:10:07 -0400

    update shell scripts

--- nld-confidence.sh
+++ nld-confidence.sh
@@ -31,6 +31,7 @@ python3 . nld-confidence data/training-models/Open-75-CE-bs256-seed${SEED}
 python3 . nld-confidence data/training-models/Open-75-GE2E-bs128-M16-seed${SEED}
 python3 . nld-confidence data/training-models/Open-75-GE2E-bs256-M16-seed${SEED}
 python3 . nld-confidence data/training-models/Open-75-GE2E-bs256-M8-seed${SEED}
+
 python3 . nld-confidence data/training-models/Permute-20-AAM-bs128-s15.0-m0.1-seed${SEED}
 python3 . nld-confidence data/training-models/Permute-20-AAM-bs256-s15.0-m0.1-seed${SEED}
 python3 . nld-confidence data/training-models/Permute-20-AAMSC-bs128-s15.0-m0.1-K10-seed${SEED}
@@ -62,13 +63,3 @@ python3 . nld-confidence data/training-models/Permute-75-CE-bs256-seed${SEED}
 python3 . nld-confidence data/training-models/Permute-75-GE2E-bs128-M16-seed${SEED}
 python3 . nld-confidence data/training-models/Permute-75-GE2E-bs256-M16-seed${SEED}
 python3 . nld-confidence data/training-models/Permute-75-GE2E-bs256-M8-seed${SEED}
-python3 . nld-confidence data/training-models/clean-AAM-bs128-s15.0-m0.1-seed${SEED}
-python3 . nld-confidence data/training-models/clean-AAM-bs256-s15.0-m0.1-seed${SEED}
-python3 . nld-confidence data/training-models/clean-AAMSC-bs128-s15.0-m0.1-K10-seed${SEED}
-python3 . nld-confidence data/training-models/clean-AAMSC-bs128-s15.0-m0.1-K3-seed${SEED}
-python3 . nld-confidence data/training-models/clean-AAMSC-bs256-s15.0-m0.1-K10-seed${SEED}
-python3 . nld-confidence data/training-models/clean-AAMSC-bs256-s15.0-m0.1-K3-seed${SEED}
-python3 . nld-confidence data/training-models/clean-CE-bs128-seed${SEED}
-python3 . nld-confidence data/training-models/clean-CE-bs256-seed${SEED}
-python3 . nld-confidence data/training-models/clean-GE2E-bs128-M4-seed${SEED}
-python3 . nld-confidence data/training-models/clean-GE2E-bs256-M4-seed${SEED}
